622ce3c619339c0d9b35dd567c12c2bd88df3ed0
Nabeel Rehemtulla <nabeelre@gmail.com>
2023-03-12 22:42:02 -0500

    new models

--- CNN_models.py
+++ CNN_models.py
@@ -170,7 +170,41 @@ def vgg6_metadata_1_2_2(image_shape=(63, 63, 3), metadata_shape=(16,)):
 
     output = Dense(1, activation='sigmoid', name='fc_out')(x)
 
-    model = keras.Model(inputs=[triplet_input, meta_input], outputs=output, name="vgg6_metadata_1_2")
+    model = keras.Model(inputs=[triplet_input, meta_input], outputs=output, name="vgg6_metadata_1_2_2")
+
+    return model
+
+
+def vgg6_metadata_1_2_3(image_shape=(63, 63, 3), metadata_shape=(16,)):
+    # Extra high dropout
+    triplet_input = keras.Input(shape=image_shape, name="triplet")
+    meta_input = keras.Input(shape=metadata_shape, name="metadata")
+
+    # Convolution branch
+    x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(63, 63, 3), name='conv1')(triplet_input)
+    x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', name='conv2')(x_conv)
+    x_conv = MaxPooling2D(pool_size=(2, 2), name='pool1')(x_conv)
+    x_conv = Dropout(0.8, name='drop_0.5')(x_conv)
+
+    x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv3')(x_conv)
+    x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv4')(x_conv)
+    x_conv = MaxPooling2D(pool_size=(4, 4), name='pool2')(x_conv)
+    x_conv = Dropout(0.8, name='drop2_0.5')(x_conv)
+
+    x_conv = Flatten()(x_conv)
+
+    # Metadata branch
+    x_meta = Dense(16, activation='relu', name='metadata_fc_1')(meta_input)
+    x_meta = Dense(32, activation='relu', name='metadata_fc_2')(x_meta)
+    
+    # Merged branch
+    x = Concatenate(axis=1)([x_conv, x_meta])
+    x = Dense(16, activation='relu', name='comb_fc_2')(x)
+    x = Dropout(0.5)(x)
+
+    output = Dense(1, activation='sigmoid', name='fc_out')(x)
+
+    model = keras.Model(inputs=[triplet_input, meta_input], outputs=output, name="vgg6_metadata_1_2_3")
 
     return model
 
