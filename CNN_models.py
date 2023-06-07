d5a519b68ac831bc7d9dc177a9546f8e425ce44a
Nabeel Rehemtulla <nabeelre@gmail.com>
2023-06-07 17:12:46 -0500

    adding mag only CNN

--- CNN_models.py
+++ CNN_models.py
@@ -35,6 +35,39 @@ def mi_cnn(config, image_shape=(63, 63, 3), metadata_shape=(16,)):
 
     return model
 
+
+def mag_cnn(config, image_shape=(63, 63, 3), metadata_shape=(1,)):
+    triplet_input = keras.Input(shape=image_shape, name="triplet")
+    meta_input = keras.Input(shape=metadata_shape, name="metadata")
+
+    # Convolution branch
+    x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(63, 63, 3), name='conv1')(triplet_input)
+    x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', name='conv2')(x_conv)
+    x_conv = MaxPooling2D(pool_size=(2, 2), name='pool1')(x_conv)
+    x_conv = Dropout(config['dropout_1'], name='drop1')(x_conv)
+
+    x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv3')(x_conv)
+    x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv4')(x_conv)
+    x_conv = MaxPooling2D(pool_size=(4, 4), name='pool2')(x_conv)
+    x_conv = Dropout(config['dropout_2'], name='drop2')(x_conv)
+
+    x_conv = Flatten()(x_conv)
+
+    # Metadata branch
+    x_meta = Dense(1, activation='relu', name='metadata_fc_1')(meta_input)
+    
+    # Merged branch
+    x = Concatenate(axis=1)([x_conv, x_meta])
+    x = Dense(16, activation='relu', name='comb_fc_2')(x)
+    x = Dropout(config['dropout_3'])(x)
+
+    output = Dense(1, activation='sigmoid', name='fc_out')(x)
+
+    model = keras.Model(inputs=[triplet_input, meta_input], outputs=output, name="mi_cnn")
+
+    return model
+
+
 # /----- ----- ----- -----/ METADATA ONLY /----- ----- ----- -----/  
 
 def fcnn(image_shape=(63, 63, 3), metadata_shape=(16,)):
