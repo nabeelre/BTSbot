e4565f26cb162ecca105eea720dc89935e1620dc
Nabeel Rehemtulla <nabeelre@gmail.com>
2023-06-12 23:54:53 -0500

    making single input CNN take config

--- CNN_models.py
+++ CNN_models.py
@@ -94,24 +94,25 @@ def fcnn(image_shape=(63, 63, 3), metadata_shape=(16,)):
 
 # /----- ----- ----- -----/ IMAGES ONLY /----- ----- ----- -----/ 
 
-def si_cnn(image_shape=(63, 63, 3)):
+def si_cnn(config, image_shape=(63, 63, 3)):
     triplet_input = keras.Input(shape=image_shape, name="triplet")
 
-    # Convolution branch
     x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(63, 63, 3), name='conv1')(triplet_input)
     x_conv = Conv2D(16, (3, 3), activation='relu', padding='same', name='conv2')(x_conv)
     x_conv = MaxPooling2D(pool_size=(2, 2), name='pool1')(x_conv)
-    x_conv = Dropout(0.25, name='drop_0.25')(x_conv)
+    x_conv = Dropout(config['dropout_1'], name='drop1')(x_conv)
 
     x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv3')(x_conv)
     x_conv = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv4')(x_conv)
     x_conv = MaxPooling2D(pool_size=(4, 4), name='pool2')(x_conv)
-    x_conv = Dropout(0.25, name='drop2_0.25')(x_conv)
+    x_conv = Dropout(config['dropout_2'], name='drop2')(x_conv)
 
     x_conv = Flatten()(x_conv)
+    x_conv = Dense(16, activation='relu', name='fc1')(x_conv)
+    x_conv = Dropout(config['dropout_3'], name='drop3')(x_conv)
 
     output = Dense(1, activation='sigmoid', name='fc_out')(x_conv)
-
+    
     model = keras.Model(inputs=triplet_input, outputs=output, name="si_cnn")
 
     return model
