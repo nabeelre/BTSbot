9ead9e9026dc66636c4db43c9a1ee6cdb33c4c91
Nabeel Rehemtulla <nabeelre@gmail.com>
2025-04-30 15:31:46 -0500

    add swin small

--- architectures_torch.py
+++ architectures_torch.py
@@ -5,7 +5,30 @@ import torch.nn as nn
 class SwinV2_t(nn.Module):
     def __init__(self, config):
         super(SwinV2_t, self).__init__()
-        self.swin = torch.hub.load("pytorch/vision", "swin_v2_t", weights="DEFAULT", progress=False)
+        self.swin = torch.hub.load(
+            "pytorch/vision", "swin_v2_t", weights="IMAGENET1K_V1", progress=False
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(1000, config['fc1_neurons']),
+            nn.Linear(config['fc1_neurons'], config['fc2_neurons']),
+            nn.ReLU(True),
+            nn.Dropout(config['dropout1']),
+            nn.Linear(config['fc2_neurons'], 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_data: torch.Tensor) -> torch.Tensor:
+        outputs = self.swin(input_data)
+        preds = self.fc(outputs)
+        return preds.view(-1)
+
+
+class SwinV2_s(nn.Module):
+    def __init__(self, config):
+        super(SwinV2_s, self).__init__()
+        self.swin = torch.hub.load(
+            "pytorch/vision", "swin_v2_s", weights="IMAGENET1K_V1", progress=False
+        )
         self.fc = nn.Sequential(
             nn.Linear(1000, config['fc1_neurons']),
             nn.Linear(config['fc1_neurons'], config['fc2_neurons']),
