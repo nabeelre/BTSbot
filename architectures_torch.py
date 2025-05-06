5aebc4466af23497190e3f88e631d229db41b84b
Nabeel Rehemtulla <nabeelre@gmail.com>
2025-05-05 22:37:26 -0500

    add larger head to swin models

--- architectures_torch.py
+++ architectures_torch.py
@@ -8,11 +8,16 @@ class SwinV2_t(nn.Module):
         self.swin = torch.hub.load(
             "pytorch/vision", "swin_v2_t", weights="IMAGENET1K_V1", progress=False
         )
-        self.swin.head = nn.Linear(self.swin.head.in_features, 1)
+        self.swin.head = nn.Sequential(
+            nn.Linear(self.swin.head.in_features, config['fc1_neurons']),
+            nn.Linear(config['fc1_neurons'], config['fc2_neurons']),
+            nn.ReLU(True),
+            nn.Dropout(config['dropout1']),
+            nn.Linear(config['fc2_neurons'], 1),
+        )
 
     def forward(self, input_data: torch.Tensor) -> torch.Tensor:
-        logits = self.swin(input_data)  # Already returns logits
-        return logits  # Shape: (batch_size, num_classes)
+        return self.swin(input_data)
 
 
 class SwinV2_s(nn.Module):
@@ -21,16 +26,13 @@ class SwinV2_s(nn.Module):
         self.swin = torch.hub.load(
             "pytorch/vision", "swin_v2_s", weights="IMAGENET1K_V1", progress=False
         )
-        self.fc = nn.Sequential(
-            nn.Linear(1000, config['fc1_neurons']),
+        self.swin.head = nn.Sequential(
+            nn.Linear(self.swin.head.in_features, config['fc1_neurons']),
             nn.Linear(config['fc1_neurons'], config['fc2_neurons']),
             nn.ReLU(True),
             nn.Dropout(config['dropout1']),
             nn.Linear(config['fc2_neurons'], 1),
-            nn.Sigmoid()
         )
 
     def forward(self, input_data: torch.Tensor) -> torch.Tensor:
-        outputs = self.swin(input_data)
-        preds = self.fc(outputs)
-        return preds.view(-1)
+        return self.swin(input_data)
