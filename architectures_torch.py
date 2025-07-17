e643f61df2ba599542b4db38b3c6528845656682
Nabeel Rehemtulla <nabeelre@gmail.com>
2025-07-17 16:21:20 -0400

    add mm ResNet

--- architectures_torch.py
+++ architectures_torch.py
@@ -234,6 +234,48 @@ class mm_ConvNeXt(nn.Module):
         return logits
 
 
+class mm_ResNet(nn.Module):
+    def __init__(self, config):
+        super(mm_ResNet, self).__init__()
+        model_kind = config.get("model_kind", "resnet18.a1_in1k")
+        num_metadata_features = len(config.get("metadata_cols", []))
+
+        # Image branch (ResNet)
+        self.resnet_backbone = timm.create_model(model_kind,
+                                                 pretrained=config.get('pretrained', True))
+        self.resnet_feature_dim = self.resnet_backbone.fc.in_features
+        # Add global pool and norm to head when using larger legacy survey images
+        self.resnet_backbone.fc = nn.Identity()
+
+        # Metadata branch
+        self.metadata_branch = nn.Sequential(
+            nn.BatchNorm1d(num_metadata_features),
+            nn.Linear(num_metadata_features, config['meta_fc1_neurons']),
+            nn.ReLU(),
+            nn.Dropout(config['meta_dropout']),
+            nn.Linear(config['meta_fc1_neurons'], config['meta_fc2_neurons']),
+            nn.ReLU()
+        )
+
+        # Combined branch
+        combined_input_features = self.resnet_feature_dim + config['meta_fc2_neurons']
+        self.combined_head = nn.Sequential(
+            nn.Linear(combined_input_features, config['comb_fc1_neurons']),
+            nn.ReLU(),
+            nn.Linear(config['comb_fc1_neurons'], config['comb_fc2_neurons']),
+            nn.ReLU(),
+            nn.Dropout(config['comb_dropout']),
+            nn.Linear(config['comb_fc2_neurons'], 1)
+        )
+
+    def forward(self, image_input: torch.Tensor, metadata_input: torch.Tensor) -> torch.Tensor:
+        image_features = self.resnet_backbone(image_input)
+        meta_features = self.metadata_branch(metadata_input)
+        combined_features = torch.cat((image_features, meta_features), dim=1)
+        logits = self.combined_head(combined_features)
+        return logits
+
+
 class um_nn(nn.Module):
     def __init__(self, config):
         super(um_nn, self).__init__()
